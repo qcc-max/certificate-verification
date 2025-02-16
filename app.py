299228d3e9dd5edc5ab1a7012ca782311fa4f6ac
qcc-max <shafeeqmehmood0@gmail.com>
2025-02-16 14:03:55 +0500

    app.py
    railway

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Railway's assigned port
+    app.run(host="0.0.0.0", port=port, debug=True)
 import hashlib
 from flask import Flask, render_template, request
 from pdf2image import convert_from_path
